f6a8c06b964f0d16d441099e159d5393f6f46f1a
Will Fisher <will@gina.alaska.edu>
2011-08-04 10:37:11 -0800

    Adding skeleton rails application

---
+++ rest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rest::Application.config.secret_token = 'e46cd6c7eb6410ff822f78ba3630966f11e4fabf1448b5912ff748627ac70ae37f29aa92b9581e7d13a36f4e02c4d4e755006e364875f6deda5f87bd7f718d4e'
