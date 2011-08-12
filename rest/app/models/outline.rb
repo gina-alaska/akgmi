d0f98c45f7f340376f6bd43a339157b6f8932d62
will <will@zap.gina.alaska.edu>
2011-08-12 15:49:23 -0800

    setup the models for publications and outlines

---
+++ rest/app/models/outline.rb
@@ -0,0 +1,17 @@
+require 'sdo_geometry'
+require 'oci8_hack'
+
+class Outline < ActiveRecord::Base
+  set_table_name 'DGGS_PUBLICATIONS.V_MAP_INDEX_OUTLINES'
+
+ 	def as_json(attrs = {})
+		{
+			:citation_id => self.citation_id,
+			:wkt => self.geometry.as_georuby.as_wkt,
+			:map_label => self.map_label,
+			:map_scale_denominator => self.map_scale_denominator,
+			:outline_source => self.outline_source,
+			:map_index_id => self.map_index_id	
+		}
+	end 
+end
---
+++ rest/app/models/publication.rb
@@ -0,0 +1,3 @@
+class Publication < ActiveRecord::Base
+  set_table_name 'DGGS_PUBLICATIONS.V_MAP_INDEX_PUBLICATIONS'
+end
