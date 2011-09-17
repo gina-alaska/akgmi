590f1395d3658021bc5b9ecca115b5aa912c9e74
Will Fisher <will@teknofire.net>
2011-09-17 11:37:56 -0700

    adding configs for the various texts used in the system so they can be changed easily down the road

--- web/public/javascripts/config.js
+++ web/public/javascripts/config.js
@@ -1,3 +1,28 @@
 CONFIG = {
-	restUrl: 'http://akgmi-rest.zap.gina.alaska.edu'
+	restUrl: 'http://akgmi-rest.zap.gina.alaska.edu',
+	text: {
+		search_form: {
+			title: 'Advanced Search',
+			search_button: 'Search',
+			advanced_button: 'Advanced',
+			reset_button: 'Clear',
+			keyword_default: 'Enter search here',
+			agency: 'Agency',
+			years: 'Year(s)',
+			quadrangles: 'Quadrangle(s)',
+			scale: 'Scale',
+			statewide: 'Include statewide records?'
+		}
+	},
+	getText: function(path) {
+		var items = path.split(/\./);
+		var text = this.text;
+		Ext.each(items, function(name) {
+			if(text[name]) { text = text[name]; }
+			else { text = null; }
+		}, this);
+		
+		if(!text) { text = 'Unknown path ' + path; }
+		return text;
+	}
 };
