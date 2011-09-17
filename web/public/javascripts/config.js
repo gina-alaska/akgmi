f1ed210c5c42c6254107d6330680b08e41f6dc47
Will Fisher <will@teknofire.net>
2011-09-17 15:13:31 -0800

    Adding a bunch for text entries for the locale text info

--- web/public/javascripts/config.js
+++ web/public/javascripts/config.js
@@ -1,6 +1,7 @@
 CONFIG = {
 	restUrl: 'http://akgmi-rest.zap.gina.alaska.edu',
-	text: {
+	defaultLang: 'EN',
+	EN: {
 		search_form: {
 			title: 'Advanced Search',
 			search_button: 'Search',
@@ -10,13 +11,44 @@ CONFIG = {
 			agency: 'Agency',
 			years: 'Year(s)',
 			quadrangles: 'Quadrangle(s)',
+			quadrangles_default: 'Select one or more quadrangles',
 			scale: 'Scale',
-			statewide: 'Include statewide records?'
+			scale_from_default: 'From:',
+			scale_to_default: 'To:',
+			statewide: 'Include statewide records?',
+			scale_to:  [
+        { key: 24000, display: '1:24,000'},
+        { key: 63600, display: '1:63,600'},
+        { key: 100000, display: '1:100,000'},
+        { key: 250000, display: '1:250,000'},
+        { key: 500000, display: '1:500,000'},
+        { key: 3500000, display: '1:3,500,000'},
+        { key: 25000000, display: '1:25,000,000'}
+      ],
+			scale_from: [
+        { key: 1, display: '1:1' },
+        { key: 24000, display: '1:24,000'},
+        { key: 63600, display: '1:63,600'},
+        { key: 100000, display: '1:100,000'},
+        { key: 250000, display: '1:250,000'},
+        { key: 500000, display: '1:500,000'},
+        { key: 3500000, display: '1:3,500,000'}
+      ]
+		},
+		results: {
+			title: 'Search Results',
+			maps: 'Maps',
+			cd_dvd: 'CD/DVD',
+			geospatial: 'Geospatial',
+			reports: 'Reports',
+			outside_links: 'Outside Links',
+			any_type: 'Any Type'
 		}
 	},
-	getText: function(path) {
+	get: function(path) {
 		var items = path.split(/\./);
-		var text = this.text;
+		var text = this[this.defaultLang];
+		
 		Ext.each(items, function(name) {
 			if(text[name]) { text = text[name]; }
 			else { text = null; }
