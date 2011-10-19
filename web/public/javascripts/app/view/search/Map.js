1d41a45094b61cab6295c750575fe81ec1a4278b
Will Fisher <will@teknofire.net>
2011-10-19 12:25:20 -0800

    Adding aoi selection tool to the map
    This just adds the UI components and is not hooked up to the searches yet for issue #22

--- web/public/javascripts/app/view/search/Map.js
+++ web/public/javascripts/app/view/search/Map.js
@@ -8,7 +8,7 @@ Ext.define('AKGMI.view.search.Map', {
   alias: 'widget.search_map',
   
   initComponent: function() {
-    this.addEvents('featureselect', 'featureunselect');
+    this.addEvents('featureselect', 'featureunselect', 'aoiadded');
     
     this.callParent(arguments);
   },
@@ -17,6 +17,21 @@ Ext.define('AKGMI.view.search.Map', {
   },
   onFeatureSelect: function(feature) {
     this.fireEvent('featureselect', feature.id, this);
+  },
+  onToolActivate: function(e) {
+    var active_id = e.object.id;
+    
+    Ext.each(this.navtoolbar.controls, function(control) {
+      if (control.id != active_id) {
+        control.deactivate();
+      }
+    }, this);
+  },
+  onAOIAdded: function(e) {
+    this.fireEvent('aoiadded', this, this.aoi.features[0]);
+  },
+  beforeAOIAdded: function(){
+    this.aoi.removeAllFeatures();
   },
   
   listeners: {
@@ -45,7 +60,43 @@ Ext.define('AKGMI.view.search.Map', {
       });
       map.getMap().addLayer(map.outlines);
       
+      var aoiStyles = new OpenLayers.StyleMap({
+        "default": new OpenLayers.Style({
+          fillColor: "#FF0000",
+          fillOpacity: 0, 
+          strokeColor: "#FF0000",
+          strokeWidth: 2,
+          strokeOpacity: 0.5
+        }),
+        "select": new OpenLayers.Style({
+          fillColor: "#FFFF00",
+          fillOpacity: 0.25,
+          strokeColor: "#FFFF00",
+          strokeWidth: 2,
+          strokeOpacity: 0.75 
+        }),  
+      });
+      map.aoi = new OpenLayers.Layer.Vector('AOI', {
+        isBaseLayer: false,
+        displayInLayerSwitcher: false,
+        styleMap: aoiStyles,
+        eventListeners: { beforefeaturesadded: Ext.bind(this.beforeAOIAdded, this) } 
+      });
+      map.getMap().addLayer(map.aoi);
+                 
       this.navtoolbar = new OpenLayers.Control.Panel();
+    
+      this.aoiSelector = new OpenLayers.Control.DrawFeature(map.aoi, OpenLayers.Handler.RegularPolygon, {
+        title: 'AOI Selection: Click and drag the mouse to define your area of interest',
+        type: OpenLayers.Control.TYPE_TOGGLE,
+        eventListeners: {
+          activate: Ext.bind(this.onToolActivate, this),
+          featureadded: Ext.bind(this.onAOIAdded, this)
+        },
+        handlerOptions: {
+          irregular: true
+        }
+      });
     
       this.featureSelector = new OpenLayers.Control.SelectFeature(map.outlines, {
         title: 'Select Publications: Click an outline to select or click and drag to select multiple outlines',
@@ -56,11 +107,14 @@ Ext.define('AKGMI.view.search.Map', {
         clickout: false,
         box: true,
         multipleKey: 'shiftKey',
+        eventListeners: {
+          activate: Ext.bind(this.onToolActivate, this)
+        },
         onSelect: Ext.bind(this.onFeatureSelect, this),
         onUnselect: Ext.bind(this.onFeatureUnselect, this)
       });
       
-      this.navtoolbar.addControls([this.featureSelector]);
+      this.navtoolbar.addControls([this.featureSelector, this.aoiSelector]);
       
       this.featureClickSelector = new OpenLayers.Control.SelectFeature(map.outlines, {
         title: 'Select Publications: Click an outline to select or click and drag to select multiple outlines',
@@ -74,6 +128,7 @@ Ext.define('AKGMI.view.search.Map', {
         onSelect: Ext.bind(this.onFeatureSelect, this),
         onUnselect: Ext.bind(this.onFeatureUnselect, this)
       });
+      
       map.getMap().addControl(this.featureClickSelector);
       this.featureClickSelector.activate();
       
