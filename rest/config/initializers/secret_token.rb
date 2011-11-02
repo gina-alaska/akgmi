b11629274a1118ff0a825c061e01dbe9e76af335
Will Fisher <will@teknofire.net>
2011-11-02 11:23:33 -0800

    Updating to rails 3.0.10 in prep for 3.1.1

--- rest/config/initializers/secret_token.rb
+++ rest/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rest::Application.config.secret_token = 'e46cd6c7eb6410ff822f78ba3630966f11e4fabf1448b5912ff748627ac70ae37f29aa92b9581e7d13a36f4e02c4d4e755006e364875f6deda5f87bd7f718d4e'
+Rest::Application.config.secret_token = '7730486e07902a12191623699abb6490b8a8aa45f202ac8cbdd0b5d00225495d328a06bd492360fc3ceb180bbe010716133c89fd279106dbf73d0cb46b7887d5'
