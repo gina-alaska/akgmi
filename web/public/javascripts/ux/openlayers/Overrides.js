e9c47b607703420169ae5c9fb59d9640605a5a18
Will Fisher <will@teknofire.net>
2011-12-07 15:25:26 -0900

    Adding comments for new minZoom functions

--- web/public/javascripts/ux/openlayers/Overrides.js
+++ web/public/javascripts/ux/openlayers/Overrides.js
@@ -1,9 +1,20 @@
+/**
+* Override for the default isValidZoomLevel function in OpenLayers.Map
+* This will allow us to specify the min zoom that is valid for each map instance.
+* 
+* Tested with OpenLayers v2.11
+**/
 OpenLayers.Map.prototype.isValidZoomLevel = function(zoomLevel) {
   return ( (zoomLevel != null) &&
            (zoomLevel >= this.getMinZoomLevel()) &&
            (zoomLevel < this.getNumZoomLevels()) );
 }
 
+/**
+* Get the minZoomLevel that is either specified on the map object or the currently
+* active base layer
+*
+**/
 OpenLayers.Map.prototype.getMinZoomLevel = function() {
   var minZoomLevel = this.minZoomLevel;
   
