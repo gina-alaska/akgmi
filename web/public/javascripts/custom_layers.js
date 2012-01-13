0e5749c6c5da88a0cefd50ab20df6c806486053b
Will Fisher <will@teknofire.net>
2012-01-12 15:39:58 -0900

    Adding back landownership with general use only message

--- web/public/javascripts/custom_layers.js
+++ web/public/javascripts/custom_layers.js
@@ -35,6 +35,25 @@ Gina.Layers.define('TILE.EPSG:3338.TOWNSHIP', {
 });
 
 /**
+ * Name: TILE.EPSG:3338.LANDOWNERSHIP
+ * Projection: EPSG:3338 Alaskan Albers
+ * Landownership layer
+ */
+Gina.Layers.define('TILE.EPSG:3338.LANDOWNERSHIP', {
+  name: 'Landownership',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://tiles.proto.gina.alaska.edu/test/tilesrv/glo/tile/${x}/${y}/${z}',
+  layerOptions: {
+    type: 'jpeg',
+    transitionEffect: 'resize',
+    wrapDateLine: false,
+    visibility: false,
+    isBaseLayer: false,
+    attribution: 'Landownership data is for general orientation use only'
+  }
+});
+
+/**
  * Name: TILE.EPSG:3338.QUADS
  * Projection: EPSG:3338 Alaskan Albers
  * Quandrangles from DGGS
