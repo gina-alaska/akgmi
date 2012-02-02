9d530e7983980b46024ce419db9958719f147db6
Will Fisher <will@teknofire.net>
2012-02-01 15:20:05 -0900

    making it so that parent tree nodes are not collapsible if a child is checked, closes #52

--- web/public/javascripts/app/view/search/Themes.js
+++ web/public/javascripts/app/view/search/Themes.js
@@ -2,7 +2,6 @@ Ext.define('AKGMI.view.search.Themes', {
   extend: 'Ext.tree.Panel',
   alias: 'widget.search_themes',
 
-  rootVisible: false,
   multiSelect: true,
   checkOnly: false,
   rootVisible: true,
@@ -10,9 +9,23 @@ Ext.define('AKGMI.view.search.Themes', {
   initComponent: function() {
     this.callParent(arguments);
     this.on('itemclick', this.clickHandler, this);
+    this.on('beforeitemcollapse', this.beforeNodeCollapse, this);
+  },
+  
+  beforeNodeCollapse: function(node){
+    var checked = false;
+    Ext.each(node.childNodes, function(n) {
+      if(n.get('checked')) {
+        checked = true;
+      }
+    }, this);
+    
+    return !checked;
   },
 
   clickHandler: function(tree, record, item, index, e, opts) {
+    var checked;
+    
     if(record.isLeaf()) {
       /* Don't mess with check status if the user clicks the check box */
       if(e.target.nodeName != 'INPUT') {
@@ -20,19 +33,20 @@ Ext.define('AKGMI.view.search.Themes', {
       }
 
       /* Check to make sure there are no other check children before we uncheck the parent */
-      var checked = true;
+      checked = true;
       Ext.each(record.parentNode.childNodes, function(node) {
         checked = checked && node.get('checked');
       }, this);
       record.parentNode.set('checked', checked);
     } else {
-      if(e.target.nodeName != 'INPUT') {
-        !record.isExpanded() ? record.expand() : record.collapse();
+      if(e.target.nodeName == 'INPUT') {
+        Ext.each(record.childNodes, function(node) {
+          node.set('checked', record.get('checked'));
+        });
+        record.expand();
+      } else {
+        record.isExpanded ? record.collapse() : record.expand();        
       }
-      
-      Ext.each(record.childNodes, function(node) {
-        node.set('checked', record.get('checked'));
-      });
     }
   },
   
@@ -41,4 +55,4 @@ Ext.define('AKGMI.view.search.Themes', {
       r.set('checked', false);
     }, this);
   }
-})
+});
